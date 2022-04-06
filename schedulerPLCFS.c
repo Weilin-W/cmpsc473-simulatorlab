606121fbffed4e29fd1510fa3eb2c6ffe5344f67
Weilin-W <wpw5138@psu.edu>
2022-04-05 22:41:54 -0400

    V0.1.8(Initialized PLCFS from LCFS, issue with PLCFSScheduleJob issue)

--- schedulerPLCFS.c
+++ schedulerPLCFS.c
@@ -7,6 +7,8 @@
 // PLCFS scheduler info
 typedef struct {
     /* IMPLEMENT THIS */
+    job_t* job;
+    list_t* queue;
 } scheduler_PLCFS_t;
 
 // Creates and returns scheduler specific info
@@ -17,6 +19,8 @@ void* schedulerPLCFSCreate()
         return NULL;
     }
     /* IMPLEMENT THIS */
+    info->queue = list_create(NULL);
+    info->job = NULL;
     return info;
 }
 
@@ -25,6 +29,7 @@ void schedulerPLCFSDestroy(void* schedulerInfo)
 {
     scheduler_PLCFS_t* info = (scheduler_PLCFS_t*)schedulerInfo;
     /* IMPLEMENT THIS */
+    list_destroy(info->queue);
     free(info);
 }
 
@@ -37,6 +42,17 @@ void schedulerPLCFSScheduleJob(void* schedulerInfo, scheduler_t* scheduler, job_
 {
     scheduler_PLCFS_t* info = (scheduler_PLCFS_t*)schedulerInfo;
     /* IMPLEMENT THIS */
+    if(info->job == NULL){
+        info->job = job;
+        list_insert(info->queue, info->job);
+        uint64_t jobCompletionTime = jobGetJobTime(info->job)+currentTime;
+        schedulerScheduleNextCompletion(scheduler, jobCompletionTime);
+    }else{
+        list_insert(info->queue, job);
+        schedulerCancelNextCompletion(scheduler);
+        uint64_t jobCompletionTime = jobGetJobTime(info->job)+currentTime;
+        schedulerScheduleNextCompletion(scheduler, jobCompletionTime);
+    }
 }
 
 // Called to complete a job in response to an earlier call to schedulerScheduleNextCompletion
@@ -50,5 +66,16 @@ job_t* schedulerPLCFSCompleteJob(void* schedulerInfo, scheduler_t* scheduler, ui
     /* IMPLEMENT THIS */
     //Using cancelComplete
     //If job currently running, lastest on preemptes the current job
-    return NULL;
+    job_t* temp = NULL;
+    list_node_t* remove_node = list_find(info->queue, info->job);
+    temp = info->job;
+    list_remove(info->queue, remove_node);
+    if(list_count(info->queue) != 0){
+        info->job = list_head(info->queue)->data;
+        uint64_t jobCompletionTime = jobGetJobTime(info->job)+currentTime;
+        schedulerScheduleNextCompletion(scheduler, jobCompletionTime);
+    }else{
+        info->job = NULL;
+    }
+    return temp;
 }
