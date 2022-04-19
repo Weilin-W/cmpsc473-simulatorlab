da57e84897e380a6c352213a8fa139e288690f0e
Weilin-W <wpw5138@psu.edu>
2022-04-19 01:27:10 -0400

    V0.2.4(PS: Following Announcement about PS policy, implementing the logic of adjusted times(**BETA**))

--- schedulerPS.c
+++ schedulerPS.c
@@ -4,9 +4,30 @@
 #include "job.h"
 #include "linked_list.h"
 
+//Job compare function:
+//Compares job data1 and job data2
+//Return 1, if job data1 time is greater than the job data 2 time
+//If equal; check job ID
+//Else Return -1
+int compare_PS_Job(void* data1, void* data2){
+    if(jobGetRemainingTime((job_t*)data1) > jobGetRemainingTime((job_t*)data2)){
+        return 1;
+    }else if(jobGetRemainingTime((job_t*)data1) == jobGetRemainingTime((job_t*)data2)){
+        if(jobGetId((job_t*)data1) > jobGetId((job_t*)data2)){
+            return 1;
+        }else{
+            return -1;
+        }
+    }else{
+        return -1;
+    }
+}
 // PS scheduler info
 typedef struct {
     /* IMPLEMENT THIS */
+    job_t* job;
+    list_t* queue;
+    uint64_t arrive_timestamp;
 } scheduler_PS_t;
 
 // Creates and returns scheduler specific info
@@ -17,6 +38,8 @@ void* schedulerPSCreate()
         return NULL;
     }
     /* IMPLEMENT THIS */
+    info->queue = list_create(compare_PS_Job);
+    info->job = NULL;
     return info;
 }
 
@@ -25,6 +48,7 @@ void schedulerPSDestroy(void* schedulerInfo)
 {
     scheduler_PS_t* info = (scheduler_PS_t*)schedulerInfo;
     /* IMPLEMENT THIS */
+    list_destroy(info->queue);
     free(info);
 }
 
@@ -37,6 +61,40 @@ void schedulerPSScheduleJob(void* schedulerInfo, scheduler_t* scheduler, job_t* 
 {
     scheduler_PS_t* info = (scheduler_PS_t*)schedulerInfo;
     /* IMPLEMENT THIS */
+    if(list_count(info->queue) == 0){
+        //Update the current job, calculate the completion time and schedule
+        info->job = job;
+        list_insert(info->queue, info->job);
+        uint64_t unaccounted_time = jobGetArrivalTime(info->job);
+        uint64_t adjust_time =  (currentTime) - info->arrive_timestamp;
+        uint64_t jobCompletionTime = (unaccounted_time + adjust_time) / list_count(info->queue);
+        info->arrive_timestamp = currentTime;
+        uint64_t jobRemainingTime = jobGetRemainingTime(info->job) - 1;
+        jobSetRemainingTime(info->job, jobRemainingTime);
+        schedulerScheduleNextCompletion(scheduler, jobCompletionTime);
+    }else{
+        //Calculate the time that the job remains, cancel the next completion
+        //Update current job, and put into queue
+        //uint64_t adjust_time = currentTime - info->arrive_timestamp;
+        list_node_t* head_node = list_head(info->queue);
+        while(head_node != NULL){
+            uint64_t unaccounted_time = jobGetArrivalTime(info->job);
+            uint64_t adjust_time =  (currentTime) - info->arrive_timestamp;
+            uint64_t jobRemainingTime = (unaccounted_time + adjust_time) % list_count(info->queue);
+
+            info->arrive_timestamp = currentTime;
+            jobSetRemainingTime(info->job, jobRemainingTime);
+            head_node = head_node->next;
+        }
+        if(info->job != NULL){
+            schedulerCancelNextCompletion(scheduler);
+        }
+        list_insert(info->queue, job);
+        info->job = list_head(info->queue)->data;
+        uint64_t adjust_time = info->arrive_timestamp - (currentTime);
+        uint64_t jobCompletionTime = (info->arrive_timestamp + adjust_time) % list_count(info->queue);
+        schedulerScheduleNextCompletion(scheduler, jobCompletionTime);
+    }
 }
 
 // Called to complete a job in response to an earlier call to schedulerScheduleNextCompletion
@@ -48,5 +106,24 @@ job_t* schedulerPSCompleteJob(void* schedulerInfo, scheduler_t* scheduler, uint6
 {
     scheduler_PS_t* info = (scheduler_PS_t*)schedulerInfo;
     /* IMPLEMENT THIS */
-    return NULL;
+    job_t* temp = NULL;
+    temp = info->job;
+    if(info->job != NULL){
+        list_node_t* infoJob_node = list_find(info->queue, info->job);
+        list_remove(info->queue, infoJob_node);
+    }
+    if(list_count(info->queue) != 0){
+        //Set the current job to the head job of the queue
+        //Remove head node of the queue
+        //Calculate the Job completion time, and schedule
+        info->job = list_head(info->queue)->data;
+        info->arrive_timestamp = currentTime;
+        uint64_t jobCompletionTime = jobGetRemainingTime(info->job)+currentTime;
+        schedulerScheduleNextCompletion(scheduler, jobCompletionTime);
+    }else{
+        info->job = NULL;
+        info->arrive_timestamp = 0;
+    }
+    //Returns the job that is being completed
+    return temp;
 }
